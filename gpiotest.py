9e8ce176c502e1f5587f63a2b9026ea3b5270e0f
Jim Schrempp <github1@jimschrempp.com>
2023-10-19 17:31:21 -0700

    gpio read

--- gpiotest.py
+++ gpiotest.py
@@ -3,6 +3,7 @@ Test the GPIO pins on the Raspberry Pi by blinking an LED
 
 
 sudo apt-get install python3-rpi.gpio
+pip install rpi.gpio
 
 """
 
@@ -14,6 +15,12 @@ GPIO.setmode(GPIO.BOARD)
 
 # Set up pin 8 as an output
 GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)
+
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+
+while True: # Run forever
+    if GPIO.input(10) == GPIO.HIGH:
+        print("Button was pushed!")
 
 # Blink the LED 5 times
 for i in range(25):
