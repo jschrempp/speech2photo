0ab19b4e2a8045d7ec8b4ead253ede0e97407c66
Jim Schrempp <github1@jimschrempp.com>
2023-10-21 14:10:21 -0700

    t

--- pyspeech.py
+++ pyspeech.py
@@ -101,44 +101,44 @@ else:
     # Set up pin 8 as an output
     GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)
 
-    # Define a function to blink the LED
-    # This function is run on a thread
-    # Communicate by putting a tuple of (onTime, offTime) in the qBlinkControl queue
-    #
-    def blink_led(q):
-        print("Starting LED thread") # why do I need to have this for the thread to work?
-        logger.info("Starting LED thread")
-        # initialize the blink time
-        onTime = 0
-        offTime = 1000  # a long time
+# Define a function to blink the LED
+# This function is run on a thread
+# Communicate by putting a tuple of (onTime, offTime) in the qBlinkControl queue
+#
+def blink_led(q):
+    print("Starting LED thread") # why do I need to have this for the thread to work?
+    logger.info("Starting LED thread")
+    # initialize the blink time
+    onTime = 0
+    offTime = 1000  # a long time
 
-        while True:
-            # Get the blink time from the queue
-            try:
-                blink_time = q.get_nowait()
-            except:
-                blink_time = None
+    while True:
+        # Get the blink time from the queue
+        try:
+            blink_time = q.get_nowait()
+        except:
+            blink_time = None
 
-            if blink_time is None:
-                # no change
-                pass
-            elif blink_time[0] == -1:
-                # stop blinking
-                GPIO.output(8, GPIO.LOW)
-                onTime = 0
-                offTime = 1000
-            else:
-                onTime = blink_time[0]
-                offTime = blink_time[1]
-
-            # Turn the LED on
-            GPIO.output(8, GPIO.HIGH)
-            # Wait for blink_time seconds
-            time.sleep(onTime)
-            # Turn the LED off
+        if blink_time is None:
+            # no change
+            pass
+        elif blink_time[0] == -1:
+            # stop blinking
             GPIO.output(8, GPIO.LOW)
-            # Wait for blink_time seconds
-            time.sleep(offTime)
+            onTime = 0
+            offTime = 1000
+        else:
+            onTime = blink_time[0]
+            offTime = blink_time[1]
+
+        # Turn the LED on
+        GPIO.output(8, GPIO.HIGH)
+        # Wait for blink_time seconds
+        time.sleep(onTime)
+        # Turn the LED off
+        GPIO.output(8, GPIO.LOW)
+        # Wait for blink_time seconds
+        time.sleep(offTime)
 
 # Create a new thread to blink the LED
 qBlinkControl = Queue()
