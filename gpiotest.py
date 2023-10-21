e3b3c1610a79e0361b96374a5abf4ccb90088380
Jim Schrempp <github1@jimschrempp.com>
2023-10-21 07:45:38 -0700

    t

--- gpiotest.py
+++ gpiotest.py
@@ -52,7 +52,6 @@ led_thread1.start()
 blink_times = [0.1, 0.1]
 while True:
     blink_times[0] += 0.1
-    blink_times[1] += 0.05
     q.put(blink_times)
     # Wait for 10 seconds
     time.sleep(5)
