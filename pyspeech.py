deb4e56e73450dec99ac9912fc8ea8c35019123c
Jim Schrempp <github1@jimschrempp.com>
2023-10-21 15:57:30 -0700

    t

--- pyspeech.py
+++ pyspeech.py
@@ -556,7 +556,7 @@ while not done:
         if firstProcessStep <= processStep.Transcribe:
         
             qBlinkControl.put(constBlinkSlow)
-            time.sleep(0.1)
+            time.sleep(5)
 
             if args.transcript == 0:
                 # transcribe the recording
