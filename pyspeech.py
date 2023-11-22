d25904e239b3ed29f35f9a939f3a18b815de21cb
Jim Schrempp <github1@jimschrempp.com>
2023-11-21 21:02:03 -0800

    test

--- pyspeech.py
+++ pyspeech.py
@@ -809,11 +809,13 @@ while not done:
                     numLoops = 1
                     isButtonPressed = True
                     lastButtonPressedTime = time.time()
+                    print("stop random displa " + str(lastButttonPressedTime)
                     randomDisplayMode = False
+
                 else:
                     # if the last button press was more than 60 seconds ago, then display history
                     if (time.time() - lastButttonPressedTime > 60) and (not randomDisplayMode):
-                        print ("switching to random display mode")
+                        print ("start random display " + str(time.time()) + str(lastButttonPressedTime))
                         lastImageDisplayedTime = time.time()
                         randomDisplayMode = True # stay in this mode until the button is pressed again
                         lastImageDisplayedTime = 0 # should display a picture immediately
