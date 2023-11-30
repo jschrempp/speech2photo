a60215d2d6dbe5687d8c5440f5db9d5aafca83d5
Jim Schrempp <github1@jimschrempp.com>
2023-11-30 09:47:08 -0800

    comments

--- s2p.sh
+++ s2p.sh
@@ -1,10 +1,14 @@
-# script to make running the speech2picture program easier
-# will be called by the autostart desktop file
-#
 #!./bin/bash
+# script to make running the speech2picture program easier
+#
+# Also could be called by s2p.desktop shortcut
+#
+# On RPi if using hardware button always, then modify the last line
+# to have the -g command line option instead of -o
 cd ~
 pwd
-# OPENAI_API_KEY is in .bashrc
+# the OPENAI_API_KEY is initialized in .bashrc
+# see comments at the top of pyspeech.py
 source ~/.bashrc
 cd speech2picture
 source /home/jbs/speech2picture/.venv/bin/activate
